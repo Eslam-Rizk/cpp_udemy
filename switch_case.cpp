475b5a85fd2425f0d97b55b8e2f0cd38eec283f0
Eslam Rizk <eslam.mahmoud18@yahoo.com>
2025-02-04 21:16:18 +0200

    switch_case

---
+++ switch_case.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    // the C++17 init-statement syntax can be helpful when there is
+    // no implicit conversion to integral or enumeration type
+    struct Device
+    {
+        enum State
+        {
+            SLEEP,
+            READY,
+            BAD
+        };
+        auto state() const { return m_state; }
+        void set_state(State state) { m_state = state; }
+
+    private:
+        State m_state{};
+    };
+
+    auto dev = Device{};
+
+    dev.set_state(Device::READY);
+    std::cout << "device state is: " << dev.state() << std::endl; // 1
+
+    switch (dev.state())
+    {
+    case Device::SLEEP:
+        std::cout << "device is in sleep mode" << std::endl;
+        break;
+    case Device::READY:
+        std::cout << "device is ready for use" << std::endl;
+        break;
+    case Device::BAD:
+        std::cout << "device is in bad state" << std::endl;
+        break;
+    }
+}
